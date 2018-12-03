f2e3d397f8e3b6ab46f26d1a68fef701bc0d5dc3
qiujie <qiujie2010@qq.com>
2018-12-03 09:42:19 +0800

    commit

--- LeetCode/10. Regular Expression Matching.py
+++ LeetCode/10. Regular Expression Matching.py
@@ -2,9 +2,20 @@
 import re
 class Solution(object):
     def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
-    
+        memo = {}
+        def dp(si, pi):
+            if pi >= len(p): return si == len(s)
+            if si >= len(s): return pi + 1 < len(p) and p[pi + 1] == '*' and dp(si, pi + 2)
+            if (si, pi) not in memo:
+                matched = p[pi] == '.' or p[pi] == s[si]
+                if pi + 1 < len(p) and p[pi + 1] == '*':
+                    memo[(si, pi)] = dp(si, pi + 2) or (matched and dp(si + 1, pi))
+                else:
+                    memo[(si, pi)] = matched and dp(si + 1, pi + 1)
+            return memo[(si, pi)]
+        return dp(0, 0)
+
+a = Solution()
+s = 'aa'
+p = 'a'
+print(a.isMatch(s,p))
