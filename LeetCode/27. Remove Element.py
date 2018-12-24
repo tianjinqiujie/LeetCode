c74fb086f2ead1f787e06f470d85ee814b3940a9
qiujie <qiujie2010@qq.com>
2018-12-24 16:43:18 +0800

    commit

---
+++ LeetCode/27. Remove Element.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def removeElement(self, nums, val):
+        """
+        Time:  O(n)
+        Space: O(1)
+        """
+        left = 0
+        right = len(nums) -1
+        while left <= right:
+            if nums[left] == val:
+                nums[left] = nums[right]
+                right -= 1
+            else:
+                left += 1
+        return left
