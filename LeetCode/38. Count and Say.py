ef54c7027730fe838d2cf077ea06ee796442512f
qiujie <qiujie2010@qq.com>
2019-02-20 14:24:10 +0800

    commit

--- LeetCode/38. Count and Say.py
+++ LeetCode/38. Count and Say.py
@@ -1,6 +1,15 @@
+import re
 class Solution:
     def countAndSay(self, n):
         """
         :type n: int
         :rtype: str
         """
+        s = '1'
+        for _ in range(n - 1):
+            s = re.sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), s)
+        return s
+
+a = Solution()
+print(a.countAndSay(6))
+print(a.countAndSay(7))
